078564a75e06e3d9d18556b70da158dffabd3935
somiasaad <somiasaad012@gmail.com>
2024-04-04 23:36:07 +0200

    update

--- databases/modeles/status.js
+++ databases/modeles/status.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const userStatusSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    status: { type: String, enum: ["happy", "angry", "sad"], required: true },
+    emotion: { type: String, enum: ["happy", "angry", "sad"], required: true },
   },
   {
     timestamps: true, // This enables createdAt and updatedAt fields automatically
