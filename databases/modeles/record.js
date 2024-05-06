18f35e5a4bc7b80f50d2049e100eee94531e1ae5
somiasaad <somiasaad012@gmail.com>
2024-05-06 23:25:15 +0300

    Update Project

---
+++ databases/modeles/record.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+// تعريف نموذج Record
+const RecordSchema = new mongoose.Schema({
+    userId: String,
+    emotion: String,
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+
+
+const Record = mongoose.model("Record", RecordSchema);
+
+export default Record;
